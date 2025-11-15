ef09f65b1edb5c72d25ee296158e5addd1b6646c
kareemzedan-dev <kareem.zedan.dev@gmail.com>
2025-11-15 20:42:06 +0200

    fet: ui finish

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class DalelElsham extends StatelessWidget {
             ],
 
             onGenerateRoute: RoutesManager.onGenerateRoute,
-            initialRoute: RoutesManager.addNewService,
+            initialRoute: RoutesManager.home,
           ),
         );
       },
